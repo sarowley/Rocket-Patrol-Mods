6bad0b10026481d8ff25da925cb7a64ddacec87d
Sean Rowley <sarowley@ucsc.edu>
2023-04-17 13:10:13 -0700

    got movement working

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
-console.log("yo whatup");
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
